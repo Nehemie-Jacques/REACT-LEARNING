4409386b5fa4a0756880fc51eafc89da09f3e574
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-06-12 21:56:38 +0100

    react-hooks-end

--- React Hooks/Custom Hooks/hooks/useFecth.js
+++ React Hooks/Custom Hooks/hooks/useFecth.js
@@ -1,30 +1,30 @@
 import { useEffect, useState } from "react";
 
-const useEffect = (url) => {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const reponse = await fetch(url);
-                if (!Response.ok) {
-                    throw new Error("Network esponse was not ok");
-                }
-                const result = await Response.json();
-                setData(result);
-            } catch (error) {
-                setError(error);
-            } finally {
-                setLoading(false);
-            }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
         }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        fetchData();
-    }, [url]);
+    fetchData();
+  }, [url]);
 
-    return { data, loading, error };
-}
+  return { data, loading, error };
+};
 
-export default useEffect;
+export default useFetch;
---
+++ React Hooks/UseId/App.jsx
@@ -0,0 +1,15 @@
+import UniqueId from "./components/UniqueId";
+
+const App = () => {
+  return (
+    <div>
+        <UniqueId />
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quos dolorem dolor alias deserunt modi magni molestias enim odio mollitia!
+        </p>
+        <UniqueId />
+    </div>
+  )
+}
+
+export default App
